366fecc93b8fb0a61de24acee58effe856a5085d
hongsamhc2 <hongsamgcp@gmail.com>
2021-01-16 23:05:28 +0900

    First Test

---
+++ practice/src/com/practice/test/FirstServlet.java
@@ -0,0 +1,23 @@
+package com.practice.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class FirstServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException{
+		System.out.println("init() ½ÇÇàµÊ");		
+	}
+	
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException{
+		System.out.println("service() ½ÇÇàµÊ");
+	}
+	
+}
