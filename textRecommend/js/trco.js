5b0183fba1ac12ece6a3f9487e62a54f261b67b7
hongsamhc2 <hongsamgcp@gamil.com>
2021-01-23 17:17:25 +0900

    #5

--- textRecommend/js/trco.js
+++ textRecommend/js/trco.js
@@ -1,21 +1,33 @@
 var textLoad = (function () {
-    const wordmaker = $('#wordmaker');
+    const wordmaker = $('#trco');
     if (!wordmaker) return false;
     var tagScript = '';
-
+    var taw = wordmaker.offset().top;
+    var tal = wordmaker.offset().left;
+    var fs = wordmaker.css('font-size');
+    console.log(fs);
     const po = () => {
         console.log('asdasds');
     };
 
     const testpop = () => {
         tagScript = '<div class="textpop"></div>';
-        wordmaker.append(tagScript);
+        wordmaker.after(tagScript);
+        $('.textpop').css({
+            'position':'absolute',
+            'top':taw,
+            'left':tal,
+            'background-color':'white'
+        });
     };
 
-    
+    const wordlist = ()=>{
+        tagScript = '<div class="wordlist"><span>asdasdasdasd</span></div>';
+        $('.textpop').append(tagScript);
+    }
 
     testpop();
-
+    wordlist();
     return {
         po: po
     }
