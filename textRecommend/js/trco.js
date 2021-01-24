3e24adaa8d6c9180560264efdf6a87b491e49f5e
hongsamhc2 <hongsamgcp@gamil.com>
2021-01-24 18:55:26 +0900

    #6

--- textRecommend/js/trco.js
+++ textRecommend/js/trco.js
@@ -1,11 +1,13 @@
 var textLoad = (function () {
-    const wordmaker = $('#trco');
+    const wordmaker = $('#trco') | {};
     if (!wordmaker) return false;
     var tagScript = '';
     var taw = wordmaker.offset().top;
     var tal = wordmaker.offset().left;
     var fs = wordmaker.css('font-size');
-    console.log(fs);
+    var cmp = wordmaker.prop('selectionStart');
+    console.log(wordmaker);
+    
     const po = () => {
         console.log('asdasds');
     };
@@ -14,21 +16,36 @@ var textLoad = (function () {
         tagScript = '<div class="textpop"></div>';
         wordmaker.after(tagScript);
         $('.textpop').css({
-            'position':'absolute',
-            'top':taw,
-            'left':tal,
-            'background-color':'white',
-            'border':'1px solid black'
+            'position': 'absolute',
+            'top': taw,
+            'left': tal,
+            'background-color': 'white',
+            'border': '1px solid black'
         });
     };
 
-    const wordlist = ()=>{
+    const wordlist = () => {
         tagScript = '<div class="wordlist"><span>asdasdasdasd</span></div>';
         $('.textpop').append(tagScript);
-    }
+    };
+
+    const checkWord = (text)=>{
+        var ko = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;
+        var en = /[a-z|A-Z|\s]/g;
+        ko = text.match(ko);
+        en = text.match(en);
+        var kol = ko.length;
+        var enl = en.length;
+
+        if(ko){
+            console.log(kol);
+            console.log(enl);
+        }
+    };
 
     testpop();
     wordlist();
+    checkWord('가가 가가가가가\nasdasd ');
     return {
         po: po
     }
