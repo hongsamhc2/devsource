5db6704a4a0d8804e44daf8c8ec59ff7a519cb8f
hongsamhc2 <hongsamgcp@gamil.com>
2021-01-29 21:41:09 +0900

    #check keyControll

--- textRecommend/js/testt.js
+++ textRecommend/js/testt.js
@@ -1,6 +1,9 @@
-(function($){
-    $.fn.mkmk=()=>{
-        keyDown();
+import * from "./keyControll";
+
+(function ($) {
+    $.fn.mkmk = () => {
+        console.log($.fn.tagName);
+        keyEvent();
         console.log('sd');
     };
 }(jQuery));
@@ -8,18 +11,72 @@
 
 // keyevent
 
-function keyDown(){
-    $(document).on('keydown','#wordmaker',(e)=>{
-        var kc = e.keyCode;
-
+function keyEvent() {
+    var kc;
+    $(document).on('keydown', '#wordmaker', (e) => {
+        kc = e.keyCode;
         console.log(kc);
-
-
-        return kc
+        upKey(kc);
     });
+    $(document).on('keyup', '#wordmaker', (e) => {
+
+    })
 }
 
-function upKey(e){
 
 
+
+
+function upKey(kc) {
+    var sel = false;
+    if (!sel) return;
+    if (kc === 38) {
+        console.log('asd');
+    };
+}
+function downKey(kc) {
+    var sel = false;
+    if (!sel) return;
+    if (kc === 40) {
+        console.log('asd');
+    };
+}
+function leftKey(kc) {
+    var sel = false;
+    if (!sel) return;
+    if (kc === 37) {
+        console.log('asd');
+    };
+}
+function rightKey(kc) {
+    var sel = false;
+    if (!sel) return;
+    if (kc === 39) {
+        console.log('asd');
+    };
+}
+function enterKey(kc) {
+    var sel = false;
+    if (!sel) return;
+    if (kc === 13) {
+        console.log('asd');
+    };
+}
+function backSpaceKey(kc) {
+    var sel = false;
+    if (!sel) return;
+    if (kc === 8) {
+        console.log('asd');
+    };
+}
+
+
+
+
+
+
+
+
+var preventEvent = (e) => {
+    e.preventEvent();
 }
