81a76f79cb4e79a9afbd876a8c03c9fbce0e2b1d
hongsamhc2 <hongsamgcp@gamil.com>
2021-01-31 19:37:53 +0900

    #2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-    console.log('asd');
+$(document).ready(function () {
+    adminLayout();
+
 })
+
+const adminLayout = () => {
+    setHeader();
+    let mainWidth;
+    let mainHeight;
+    let navWidth;
+    let navHeight;
+    let footerWidth;
+    let footerHeight;
+}
+
+const setHeader = (h, w) => {
+    let header = document.getElementsByTagName('header');
+    (header.length === 0) ? console.log('Tag:Header is undefined'): header = header[0];
+    header.style.height = h || '50px';
+    header.style.width = w || '100%';
+}
+
+const setNav = () => {}
+const setMain = () => {}
+const setFooter = () => {}
