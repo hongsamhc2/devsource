49ce24dbcaa8249909dd8744355180c468e6bf8d
hongsamhc2 <hongsamgcp@gmail.com>
2021-02-15 01:54:38 +0900

    #2

--- HelloWithCode/Program.cs
+++ HelloWithCode/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWithCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ere");
         }
     }
 }
