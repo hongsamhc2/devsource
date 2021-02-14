725db5f8ecb9258e4bd396763c0ab3b680f53ee4
hongsamhc2 <hongsamgcp@gmail.com>
2021-02-15 01:52:37 +0900

    #day1

---
+++ HelloWithCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWithCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
