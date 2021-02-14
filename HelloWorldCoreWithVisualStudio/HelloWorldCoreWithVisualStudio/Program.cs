0d1a04ac1d82ebe0701b0378dff1698835eb8640
hongsamhc2 <hongsamgcp@gmail.com>
2021-02-15 02:11:02 +0900

    #4

---
+++ HelloWorldCoreWithVisualStudio/HelloWorldCoreWithVisualStudio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldCoreWithVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
